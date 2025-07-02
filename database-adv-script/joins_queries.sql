b3f5bdc21ac672c47e7cb48e9403fb19c26c5814
Denen <awardenen@gmail.com>
2025-07-02 20:26:28 +0100

    Updated joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -12,7 +12,8 @@ ON b.user_id = u.user_id;
 
 SELECT p.*, r.* FROM Property AS p
 LEFT JOIN Review AS r 
-ON p.property_id = r.property_id;
+ON p.property_id = r.property_id
+ORDER BY p.property_id;
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings,
 -- even if the user has no booking or a booking is not linked to a user.
