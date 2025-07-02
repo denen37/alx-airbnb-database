8eae98425c3d7deba97fa0e673fc83e6850f3630
Denen <awardenen@gmail.com>
2025-07-02 20:23:11 +0100

    Queries with Joins

---
+++ database-adv-script/README.md
@@ -0,0 +1,10 @@
+## TABLE JOINS
+
+Table joins are used to combine rows from two or more tables, based on a related column between them.
+
+### Types of Joins
+
+- **Inner Join**: Retrieves records with matching values in both tables.
+- **Left Join**: Retrieves all records from the left table and matching records from the right table.
+- **Right Join**: Retrieves all records from the right table and matching records from the left table.
+- **Full Outer Join**: Retrieves records when there is a match in either table.
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,22 @@
+-- Write a query using an INNER JOIN to retrieve all bookings and the respective users 
+-- who made those bookings.
+
+
+SELECT b.*, u.* FROM Booking AS b 
+INNER JOIN User AS u 
+ON b.user_id = u.user_id;
+
+
+-- Write a query using a LEFT JOIN to retrieve all properties and their reviews, 
+-- including properties that have no reviews
+
+SELECT p.*, r.* FROM Property AS p
+LEFT JOIN Review AS r 
+ON p.property_id = r.property_id;
+
+-- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings,
+-- even if the user has no booking or a booking is not linked to a user.
+
+SELECT u.*, b.* FROM User AS u
+FULL OUTER JOIN Booking AS b
+ON u.user_id = b.user_id;
