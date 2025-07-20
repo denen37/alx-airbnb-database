137389b91f0eb6daaeab41e2438255ff6b576e2f
Denen <awardenen@gmail.com>
2025-07-20 20:02:44 +0100

    Updated

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,6 +1,12 @@
 CREATE INDEX idx_created_at ON booking(created_at);
 
+EXPLAIN ANALYZE SELECT * FROM booking where created_at = '2025-07-06 03:27:37';
+
 CREATE INDEX idx_price_per_night ON property(price_per_night);
 
+ EXPLAIN ANALYZE SELECT * FROM property WHERE price_per_night = 120;
+
 CREATE UNIQUE INDEX idx_phone_number ON user(phone_number);
+
+EXPLAIN ANALYZE SELECT * FROM user WHERE phone_number = '123-456-7890';
 
